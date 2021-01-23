fcb12e17a7d9c98ba4e1cac890adc2dbf479130a
Jesiel Borges <49598899+jbside@users.noreply.github.com>
2021-01-23 09:10:07 -0300

    Delete Aula01 directory

--- Aula01/src/br/com/apex/aula04/poo/Cachorro.java
+++
@@ -1,9 +0,0 @@
-package br.com.apex.aula04.poo;
-
-public class Cachorro extends Animal {
-	
-	public void comer() {
-		System.out.println("Cachorro comendo");
-	}
-
-}
--- Aula01/src/br/com/apex/aula05/exercicios/Veiculo.java
+++
@@ -1,9 +0,0 @@
-package br.com.apex.aula05.exercicios;
-
-public abstract class Veiculo {
-	
-	public abstract float acelerar();
-	
-	public abstract void parar();
-
-}
